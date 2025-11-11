cef66a9e645cfeeeddc438de8fc55d2d2660aa79
haru-094 <anas.k7aleed@gmail.com>
2025-11-11 09:53:15 +0200

    add read_file functino

--- expenses-tracker.py
+++ expenses-tracker.py
@@ -1,32 +1,43 @@
 # function to show the menu
 
 def show_menu():
-    choice = int(input(
-'''
-1) check the remaning balance
-2) View Expenseve
-3) Add New Expenseve
-4) Exit
-'''
-))
-    match choice:
-        case 1:
-            pass
-        case 2:
-            pass
-        case 3:
-            pass
-        case 4:
-            print("Exist from the program")
-            exit
-        case _:
-            print("Invaild Choice, try again...")
+    while True:
+        choice = int(input(
+            '''
+        1) check the remaning balance
+        2) View Expenseve
+        3) Add New Expenseve
+        4) Exit
+        '''
+        ))
 
-# show_menu()
+        match choice:
+            case 1:
+                read_file('balance.txt')
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                print("Exit from the program")
+                break
+            case _:
+                print("Invaild Choice, try again...")
+
 
 # read data from file.txt
 
-def read_file():
-    pass
+
+def read_file(filename):
+    try:
+        file = open(filename, 'r')
+        file_content = file.read()
+        print(file_content)
+    except:
+        pass
+    finally:
+        file.close()
 
 
+# read_file('balance.txt')
+# show_menu()
