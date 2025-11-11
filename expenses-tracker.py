932b9b4dbd0ae117d229ffeea9b7f27b913296a3
haru-094 <a.nasir@alustudent.com>
2025-11-11 16:10:20 +0200

    make some edit

--- expenses-tracker.py
+++ expenses-tracker.py
@@ -1,19 +1,48 @@
-# function to show the menu
 
-def show_menu():
+# Get the balance file path
+balance_file_path = 'balance.txt'
+
+# function to read the balance from balance file
+
+
+def read_file():
+    total = 0
+    try:
+        with open(balance_file_path) as f:
+            total = float(f.read())
+            print(total)
+    except:
+        pass
+    finally:
+        f.close()
+
+
+def update_balance(new_balance):
+    try:
+        with open(balance_file_path, 'w') as f:
+            f.write(str(new_balance))
+    except IOError:
+        print("Error: file not found, or there is error in update the amount")
+
+
+def add_new_amount(new_balance):
+    pass
+
+
+if __name__ == '__main__':
     while True:
         choice = int(input(
             '''
-        1) check the remaning balance
-        2) View Expenseve
-        3) Add New Expenseve
-        4) Exit
-        '''
+            1) check the remaning balance
+            2) View Expenseve
+            3) Add New Expenseve
+            4) Exit
+            '''
         ))
 
         match choice:
             case 1:
-                read_file('balance.txt')
+                read_file()
             case 2:
                 pass
             case 3:
@@ -23,31 +52,3 @@ def show_menu():
                 break
             case _:
                 print("Invaild Choice, try again...")
-
-
-# read data from file.txt
-
-
-def read_file(filename):
-    try:
-        file = open(filename, 'r')
-        file_content = file.read()
-        print(file_content)
-        print("Do you want to add new amount? [Yes, No]\n")
-
-    except:
-        pass
-    finally:
-        file.close()
-
-
-def update_balance(new_balance):
-    try:
-        with open('balance.txt', 'w') as f:
-            f.write(str(new_balance))
-    except IOError:
-        print("Error: file not found, or there is error in update the amount")
-
-
-def add_new_amount(new_balance):
-    pass
