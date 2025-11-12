0d672914da8a108ffb6e5d8074b01e45c2cd6f1f
haru-094 <anas.k7aleed@gmail.com>
2025-11-12 20:17:02 +0200

    complete the function add error handle in some function

--- expenses-tracker.py
+++ expenses-tracker.py
@@ -9,11 +9,23 @@ balance_file_path = 'balance.txt'
 # function to read the balance from balance file
 
 
-def read_file():
-    total = 0
+def read_balance_file():
+
+    # create the file if not exist
+    if not os.path.exists(balance_file_path):
+        try:
+            with open(balance_file_path, 'w') as f:
+                f.write('0.0')
+        except IOError:
+            print("can't create the file")
+            sys.exit(1)
+        return 0.0
+    # read the amount form the
     try:
         with open(balance_file_path, 'r') as f:
             total = f.read().strip()
+            if not total:
+                return 0.0
             return float(total)
     except IOError:
         print("Error, can't read the balance file")
@@ -45,20 +57,21 @@ def read_total_expenses():
                         if len(element) >= 4:
                             total += float(element[-1])
                     except (ValueError, IndexError):
-                        print("can't read the total amount")
+                        # print("can't read the total amount")
+                        continue
         except IOError:
             print(f"can't read the {file_name} file")
     return total
 
 
-def read_balance():
-    balance = read_file()
+def read_balance_amount():
+    balance = read_balance_file()
     expenses = read_total_expenses()
     return balance - expenses
 
 
 def current_balance():
-    balance = read_file()
+    balance = read_balance_file()
     expenses = read_total_expenses()
     remain_balance = balance - expenses
     print("Your Current Balance")
@@ -70,9 +83,13 @@ Available balance: ${remain_balance:.2f}
 
     # ask the uesr if he want to add money
     while True:
-        add_money = input("Do you want to add money? [Y/N]").lower()
+        add_money = input("Do you want to add money? [Y/N]\n").lower()
         if add_money == 'y':
-            amount = float(input("Enter the amount: "))
+            try:
+                amount = float(input("Enter the amount: \n").strip())
+            except ValueError:
+                print("amount is not number")
+                continue
             if amount <= 0:
                 print("the amount must be positive")
                 continue
@@ -87,32 +104,180 @@ Available balance: ${remain_balance:.2f}
             print("Invaild choice only Y or N")
 
 
-def add_new_amount(new_balance):
-    pass
+def add_new_expense():
+    balance = read_balance_amount()
+    print("Add New Expenses")
+    print(f"Your current balance {balance}")
+
+    while True:
+        date = input("Enter the data in format of YYYY-MM-DD: \n")
+        try:
+            datetime.strptime(date, '%Y-%m-%d')
+            break
+        except ValueError:
+            print("Invaild date must be in this format YYYY-MM-DD")
+
+    name = input("Enter the name of item: \n")
+
+    if not name:
+        print("Error, the item must not be empty")
+        return
+
+    while True:
+        try:
+            price = float(input("Enter the item price: \n"))
+            if price <= 0:
+                print("The amount must be positive")
+                continue
+            else:
+                break
+        except ValueError:
+            print("Error, the amount must be integer")
+
+    print("confirmation [y/n]\n")
+    print(f"""
+    item name: {name}
+    amount: {price:.2f}
+    date: {date}
+    """)
+    confirm = input("Are the data correct? [Y/N] \n").strip().lower()
+
+    if confirm == 'y':
+        if price > balance:
+            print("Error, Insufficient balance! Cannot save expense.")
+            return
+        file_name = f"expenses-{date}.txt"
+        item_id = 1
+        if os.path.exists(file_name):
+            try:
+                with open(file_name, 'r') as f:
+                    item_id = len(f.readlines()) + 1
+            except IOError:
+                print("Error, can't create unique ID")
+        current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        item_expense = f"{item_id},{current_date},{name},{price:.2f}\n"
+
+        # create or append the item into file
+        try:
+            with open(file_name, 'a') as f:
+                f.write(item_expense)
+        except IOError:
+            print("Error, can't save the file")
+            return
+        # init_balance = read_balance_file()
+        # new_balance = init_balance - price
+        # update_balance(new_balance)
+
+        remain_balance = read_balance_amount()
+        print("Expense added")
+        print(f"The remaining balance: {remain_balance:.2f}")
+    elif confirm == 'n':
+        print("Not adding")
+    else:
+        print("Invaild choice, enter Y or N")
 
 
-def search_items():
-    pass
+def view_search_expenses():
+    print("""
+1. Search by item name
+2. Search by amount
+3. Back to main menu
+        """)
+    while True:
+        try:
+            choice = int(input("choice from [1-3]\n"))
+        except ValueError:
+            print("Error, need to be from 1 to 3")
+            continue
+        # search for item name
+        if choice == 1:
+            item_name = input("Enter the name of item: \n").strip().lower()
+            if item_name:
+                print(f"Match found of {item_name}")
+                match_found = 0
+                expenses_files = []
+                for i in os.listdir('.'):
+                    if i.startswith('expenses') and i.endswith('.txt'):
+                        expenses_files.append(i)
+
+                for file_name in expenses_files:
+                    try:
+                        with open(file_name, 'r') as f:
+                            for content in f:
+                                element = content.strip().split(',')
+                                if len(element) >= 4:
+                                    item = element[2].lower()
+                                    if item_name in item:
+                                        print(
+                                            f"{file_name} : {content.strip()}")
+                                        match_found += 1
+                    except IOError:
+                        print(f"can't read the {file_name} file")
+                if match_found == 0:
+                    print("item not found")
+            else:
+                print("item must not be empty")
+        # search for the item price
+        elif choice == 2:
+            try:
+                price_item = float(
+                    input("Enter the amount of item: \n").strip())
+            except ValueError:
+                print("the amount need to be number")
+                continue
+            print(f"Match found of {price_item}")
+            match_found = 0
+            expenses_files = []
+            for i in os.listdir('.'):
+                if i.startswith('expenses') and i.endswith('.txt'):
+                    expenses_files.append(i)
+
+            for file_name in expenses_files:
+                try:
+                    with open(file_name, 'r') as f:
+                        for content in f:
+                            try:
+                                element = content.strip().split(',')
+                                if len(element) >= 4:
+                                    price = float(element[-1])
+                                    if price == price_item:
+                                        print(
+                                            f"{file_name} : {content.strip()}")
+                                        match_found += 1
+                            except ValueError:
+                                continue
+                except IOError:
+                    print(f"can't read the {file_name} file")
+            if match_found == 0:
+                print("item not found")
+        # exit from the menu and return the main menu
+        elif choice == 3:
+            break
+        else:
+            print("Error, only choice from 1 to 3")
 
 
 if __name__ == '__main__':
     while True:
-        choice = int(input(
-            '''
+        try:
+            choice = int(input(
+                '''
 1) check the remaning balance
 2) View Expenseve
 3) Add New Expenseve
 4) Exit
 '''
-        ))
-
+            ).strip())
+        except ValueError:
+            print("Error must be from 1 to 4 only")
+            continue
         match choice:
             case 1:
                 current_balance()
             case 2:
-                pass
+                view_search_expenses()
             case 3:
-                pass
+                add_new_expense()
             case 4:
                 print("Exit from the program")
                 break
