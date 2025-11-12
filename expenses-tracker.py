a3220668464326edc9d96e97cfcbfba15aacc0d9
haru-094 <anas.k7aleed@gmail.com>
2025-11-12 09:40:21 +0200

    add current balance , read balance function

--- expenses-tracker.py
+++ expenses-tracker.py
@@ -1,3 +1,7 @@
+import os
+import sys
+from datetime import datetime
+
 
 # Get the balance file path
 balance_file_path = 'balance.txt'
@@ -8,16 +12,16 @@ balance_file_path = 'balance.txt'
 def read_file():
     total = 0
     try:
-        with open(balance_file_path) as f:
-            total = float(f.read())
-            print(total)
-    except:
-        pass
-    finally:
-        f.close()
+        with open(balance_file_path, 'r') as f:
+            total = f.read().strip()
+            return float(total)
+    except IOError:
+        print("Error, can't read the balance file")
+        sys.exit(1)
 
 
 def update_balance(new_balance):
+
     try:
         with open(balance_file_path, 'w') as f:
             f.write(str(new_balance))
@@ -25,7 +29,69 @@ def update_balance(new_balance):
         print("Error: file not found, or there is error in update the amount")
 
 
+def read_total_expenses():
+    total = 0
+    expenses_files = []
+    for i in os.listdir('.'):
+        if i.startswith('expenses') and i.endswith('.txt'):
+            expenses_files.append(i)
+
+    for file_name in expenses_files:
+        try:
+            with open(file_name, 'r') as f:
+                for content in f:
+                    try:
+                        element = content.strip().split(',')
+                        if len(element) >= 4:
+                            total += float(element[-1])
+                    except (ValueError, IndexError):
+                        print("can't read the total amount")
+        except IOError:
+            print(f"can't read the {file_name} file")
+    return total
+
+
+def read_balance():
+    balance = read_file()
+    expenses = read_total_expenses()
+    return balance - expenses
+
+
+def current_balance():
+    balance = read_file()
+    expenses = read_total_expenses()
+    remain_balance = balance - expenses
+    print("Your Current Balance")
+    print(f'''
+Initial/current balance: ${balance:.2f}
+Total expenses to date: ${expenses:.2f}
+Available balance: ${remain_balance:.2f}
+    ''')
+
+    # ask the uesr if he want to add money
+    while True:
+        add_money = input("Do you want to add money? [Y/N]").lower()
+        if add_money == 'y':
+            amount = float(input("Enter the amount: "))
+            if amount <= 0:
+                print("the amount must be positive")
+                continue
+            new_amount = balance + amount
+            update_balance(new_amount)
+            print("The new amount successful added.")
+            print(f"The new balance: {new_amount:.2f}")
+            break
+        elif add_money == 'n':
+            break
+        else:
+            print("Invaild choice only Y or N")
+
+
 def add_new_amount(new_balance):
+    pass
+
+
+def search_items():
     pass
 
 
@@ -33,16 +99,16 @@ if __name__ == '__main__':
     while True:
         choice = int(input(
             '''
-            1) check the remaning balance
-            2) View Expenseve
-            3) Add New Expenseve
-            4) Exit
-            '''
+1) check the remaning balance
+2) View Expenseve
+3) Add New Expenseve
+4) Exit
+'''
         ))
 
         match choice:
             case 1:
-                read_file()
+                current_balance()
             case 2:
                 pass
             case 3:
