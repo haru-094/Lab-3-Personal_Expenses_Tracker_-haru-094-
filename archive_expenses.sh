1feb44ce671a95df3b19eff277fad9d13db9c0e3
haru-094 <anas.k7aleed@gmail.com>
2025-11-18 18:16:13 +0200

    fix some error and add handel error

--- archive_expenses.sh
+++ archive_expenses.sh
@@ -10,51 +10,58 @@ echo "2. Search for file"
 echo "3. Exit the program"
 read -p "choose 1-3: " user_input
 
-if [ $user_input -eq 1 ]; then
+if [ "$user_input" -eq 1 ]; then
 
     # check if the archive exist
-    if [ ! -d $archives ]; then
-        mkdir -p $archives
+    if [ ! -d "$archives" ]; then
+        mkdir -p "$archives"
         echo "Dir Created"
     fi
 
-    if ! ls expenses-*.txt 1> /dev/null 2>&1: then
+    if ! ls expenses-*.txt 1> /dev/null 2>&1; then
         echo "There is no expenses file..."
         exit 0
     fi
 
     # archiving the file and register in the log file
     for expenses_file in expenses-*.txt; do
-        if [ -f $expenses_file ]; then
-            moving_dir=$(date +"$Y-%m-%d")
+        if [ -f "$expenses_file" ]; then
+            moving_dir=$(date +"%Y-%m-%d")
             mkdir -p "$archives/$moving_dir"
             mv "$expenses_file" "$archives/$moving_dir"
-            echo "$expenses_file get archived in Time: $(date +%Y-%m-%d)" >> $log_files
+            echo "$expenses_file get archived in Time: $(date +%Y-%m-%d)" >> "$log_files"
+            echo "the file $expenses_file sucesssful archived"
         else
             echo "there is error in the file $expenses_file"
         fi
     done
 
 # searching
-elif [ $user_input -eq 2 ]; then
+elif [ "$user_input" -eq 2 ]; then
 
+    if [ ! -d "$archives" ]; then
+        echo "The archiving dir does not exist"
+        exit 0
+    fi
+    
     # check if log file exist 
-    if [ ! -f $log_files ]; then
-        touch $log_files
+    if [ ! -f "$log_files" ]; then
+        touch "$log_files"
     fi
 
-    read -p "Please enter the date that you want to search for: " user_input_date
-    if [[ ! $user_input_date =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
+    read -p "enter the date in this format(YYYY-mm-dd) that you want to search for: " user_input_date
+    if [[ ! "$user_input_date" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
         echo "This date is invaild!"
-        echo "must be in format of YYYY-mm-dd"
+        echo "You must enter on this format YYYY-mm-dd"
         exit 1
     fi
 
     if [ -d "$archives/$user_input_date" ]; then
-        for search_file in expenses-*.txt; do
-            if [ -f $search_file ]; then
+        for search_file in "$archives/$user_input_date"/expenses-*.txt; do
+            if [ -f "$search_file" ]; then
                 echo "filename $(basename "$search_file")"
-                cat $search_file
+                echo "######"
+                cat "$search_file"
                 echo "######"
             else
                 echo "NO file $search_file"
@@ -63,9 +70,9 @@ elif [ $user_input -eq 2 ]; then
     else
         echo "NO file with this date"
     fi
-elif [ $user_input -eq 3 ]; then
+elif [ "$user_input" -eq 3 ]; then
     echo "Exit..."
     exit 0
 else
-    echo "Please choose from 1-3"
+    echo "You only chooose from this range [ 1-3 ]"
 fi
